d0ad46a4ba7d1b5396a37af1500a8db7ce2be195
Martin Lehoux <martin@lehoux.net>
2020-04-04 14:50:08 -0700

    feat - LinearBlend

--- color.go
+++ color.go
@@ -7,3 +7,11 @@ type Color struct {
 func (color Color) ToPixel() Pixel {
 	return Pixel{int(color.r * 255), int(color.g * 255), int(color.b * 255)}
 }
+
+func LinearBlend(color1 Color, color2 Color, param float64) Color {
+	return Color{
+		param*color1.r + (1.0-param)*color2.r,
+		param*color1.g + (1.0-param)*color2.g,
+		param*color1.b + (1.0-param)*color2.b,
+	}
+}
