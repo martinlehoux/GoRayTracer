fa4beda1fb62fbeb69ae2371dd47239b3f7ad3aa
Martin Lehoux <martin@lehoux.net>
2020-04-04 14:32:18 -0700

    feat - Basic Vec3D

---
+++ vec3d.go
@@ -0,0 +1,47 @@
+package main
+
+import "math"
+
+type Vec3D struct {
+	x, y, z float64
+}
+
+func (vec Vec3D) Invert() Vec3D {
+	return Vec3D{-vec.x, -vec.y, -vec.z}
+}
+
+func (vec Vec3D) Length() float64 {
+	return math.Sqrt(DotProduct(vec, vec))
+}
+
+func (vec Vec3D) Unit() Vec3D {
+	length := vec.Length()
+	if length == 0 {
+		return vec
+	}
+	return ScalarProduct(1.0/length, vec)
+}
+
+func AddVec3D(vec1 Vec3D, vec2 Vec3D) Vec3D {
+	return Vec3D{vec1.x + vec2.x, vec1.y + vec2.y, vec1.z + vec2.z}
+}
+
+func SubVec3D(vec1 Vec3D, vec2 Vec3D) Vec3D {
+	return AddVec3D(vec1, vec2.Invert())
+}
+
+func ScalarProduct(scalar float64, vec Vec3D) Vec3D {
+	return Vec3D{scalar * vec.x, scalar * vec.y, scalar * vec.z}
+}
+
+func DotProduct(vec1 Vec3D, vec2 Vec3D) float64 {
+	return vec1.x*vec2.x + vec1.y*vec2.y + vec1.z*vec2.z
+}
+
+func CrossProduct(vec1 Vec3D, vec2 Vec3D) Vec3D {
+	return Vec3D{
+		vec1.y*vec2.z - vec1.z*vec2.y,
+		vec1.z*vec2.x - vec1.x*vec2.z,
+		vec1.x*vec2.y - vec1.y*vec2.x,
+	}
+}
